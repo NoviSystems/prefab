bb8b6a47f021aec44d334d75573bc5324fbb933a
Ryan P Kilby <rpkilby@ncsu.edu>
2017-01-25 08:31:19 -0500

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='prefab',
-    version='0.0.1',
+    version='0.0.2',
     license='BSD License',
 
     description='More tools for writing awesome Fabric files',
